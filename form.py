636d55056f571968a0729552e529dd6b5c7e0dba
Igor Zygin <i.zygin@tinkoff.ru>
2024-05-30 19:32:41 +0500

    requirements

--- form.py
+++ form.py
@@ -50,7 +50,7 @@ class MainForm(npyscreen.ActionForm):
         self.parentApp.setNextForm(None)
 
 def run_loop():
-    os.system('cls')
+    os.system('clear')
     while True:
         r = requests.get('http://mark2.oulu.io/api/v1/entries/current.json', timeout=5)
         js = r.json()
