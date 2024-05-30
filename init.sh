235c62e2abdfa6ec403b7016adf084e84b51ec6c
Igor Zygin <i.zygin@tinkoff.ru>
2024-05-30 19:12:48 +0500

    requirements

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
-pkg install termux-api git
+pkg install -y termux-api git python
+pkg rei -y libexpat
 termux-api-start
+pip install -r requirements.txt
+cp start.sh /data/data/com.termux/files/home/.shortcuts/start.sh
+cp stop.sh /data/data/com.termux/files/home/.shortcuts/stop.sh
---
+++ start.sh
@@ -0,0 +1,3 @@
+cd /data/data/com.termux/files/home/termux-sugar-caller && git pull && bash init.sh
+pip install -r /data/data/com.termux/files/home/termux-sugar-caller/requirements.txt
+python /data/data/com.termux/files/home/termux-sugar-caller/main.py
