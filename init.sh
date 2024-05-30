15cdb3dd484cd3e8442ea24dac7e71f59569e2b8
Igor Zygin <i.zygin@tinkoff.ru>
2024-05-30 19:15:27 +0500

    requirements

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-pkg install -y termux-api git python
-pkg rei -y libexpat
+pkg install -y termux-api git python libexpat
 termux-api-start
 pip install -r requirements.txt
+mkdir -p /data/data/com.termux/files/home/.shortcuts
 cp start.sh /data/data/com.termux/files/home/.shortcuts/start.sh
 cp stop.sh /data/data/com.termux/files/home/.shortcuts/stop.sh
