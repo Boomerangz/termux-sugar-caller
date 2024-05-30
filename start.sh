1a3ea4241d69770ddae11a922f74097d0b566f1c
Igor Zygin <i.zygin@tinkoff.ru>
2024-05-30 19:14:30 +0500

    requirements

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 cd /data/data/com.termux/files/home/termux-sugar-caller && git pull && bash init.sh
 pip install -r /data/data/com.termux/files/home/termux-sugar-caller/requirements.txt
+
+termux-wake-lock
 python /data/data/com.termux/files/home/termux-sugar-caller/main.py
+termux-wake-unlock
