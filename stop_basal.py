0b99932300a1d7d65900d66704085c4c65be3bff
Igor Zygin <i.zygin@tbank.ru>
2024-10-25 17:27:43 +0300

    improve

--- stop_basal.py
+++ stop_basal.py
@@ -156,6 +156,7 @@ def process_incoming_sms():
 def send_sms_and_approve(recipient, message):
     global awaiting_response, alert_sent_time, last_alert_time
     send_sms(recipient, message)
+    time.sleep(15)
     current_time = datetime.now()
     last_alert_time = current_time
     alert_sent_time = current_time
